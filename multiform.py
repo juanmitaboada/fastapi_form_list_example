8d1e9e7ac74feb34a7f7f6093ead3e214bfa41cd
Juanmi Taboada <juanmi@juanmitaboada.com>
2023-09-18 13:31:58 +0200

    Added GET examples

--- multiform.py
+++ multiform.py
@@ -1,18 +1,32 @@
 from typing import List
-from fastapi import FastAPI, Form, Depends
+from fastapi import FastAPI, Form, Depends, Query
 from pydantic import BaseModel
+from typing import Annotated
 
 app = FastAPI()
 
 
 """
-OPTION 1: FIELD BY FIELD
+OPTION 1A: GET - FIELD BY FIELD
+"""
+
+
+@app.get("/submit1")
+async def submit1_get(
+    field: Annotated[List[str], Query(alias="field[]")],
+    anotherfield: str = "SUBMIT1GET",
+):
+    return {"field": field, "anotherfield": anotherfield}
+
+
+"""
+OPTION 1B: POST - FIELD BY FIELD
 """
 
 
 @app.post("/submit1")
-async def submit1(
-    field: List[str] = Form(alias="field[]"), anotherfield: str = "SUBMIT1"
+async def submit1_post(
+    field: List[str] = Form(alias="field[]"), anotherfield: str = "SUBMIT1POST"
 ):
     return {"field": field, "anotherfield": anotherfield}
 
@@ -24,19 +38,47 @@ OPTION 2: USING A PYDANTIC MODEL (https://stackoverflow.com/a/70644518/1481040)
 
 class MyForm(BaseModel):
     field: List[str]
-    anotherfield: str = "SUBMIT 2"
+    anotherfield: str = "SUBMIT2GET"
 
+
+"""
+OPTION 2A: GET - USING PYDANTIC
+"""
+
+
+class MyFormGet(MyForm):
+    @classmethod
+    def as_form(
+        cls,
+        field: List[str] = Query(alias="field[]"),
+        anotherfield: str = "SUBMIT2GET",
+    ):
+        return cls(field=field, anotherfield=anotherfield)
+
+
+@app.get("/submit2")
+async def submit2_get(myform: MyFormGet = Depends(MyFormGet.as_form)):
+    print(myform.field)
+    return {"field": myform.field, "anotherfield": myform.anotherfield}
+
+
+"""
+OPTION 2B: POST - USING PYDANTIC
+"""
+
+
+class MyFormPost(MyForm):
     @classmethod
     def as_form(
         cls,
         field: List[str] = Form(alias="field[]"),
-        anotherfield: str = "SUBMIT 2",
+        anotherfield: str = "SUBMIT2POST",
     ):
         return cls(field=field, anotherfield=anotherfield)
 
 
 @app.post("/submit2")
-async def submit2(myform: MyForm = Depends(MyForm.as_form)):
+async def submit2_post(myform: MyFormPost = Depends(MyFormPost.as_form)):
     print(myform.field)
     return {"field": myform.field, "anotherfield": myform.anotherfield}
 
